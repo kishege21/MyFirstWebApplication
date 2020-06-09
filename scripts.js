a55950ff2a825346b696bd459c653a2cbd7367bd
kishege21 <kishege21@gmail.com>
2020-06-09 17:25:24 +0200

    array, objects ready_day2_final

--- scripts.js
+++ scripts.js
@@ -1,21 +1,38 @@
-let data = {
+let currentPhoto = 0;
+
+let imagesData = [
+{
     photo: "IMG_1.jpg",
     title: "San Francisco utcáin",
     description: "San Francisco-ban közkedvelt közösségi közlekedési eszköz a Cabel Car. A várost 3 fő vonal szeli át, melynek központi irányítását egyetlen, földalatti központból irányítják."
-};
-
-$("#photo").attr("src", data.photo);
-$("#photo-title").append(data.title);
-$("#photo-description").append(data.description);
-
-let currentPhoto = 0;
-
-let imagesData = {
-    photo: ["IMG_1.jpg", "IMG_2.jpg", "IMG_3.jpg", "IMG_4.jpg", "IMG_5.jpg", "IMG_6.jpg"];
-    title: ["San Francisco utcain", "Golden-Gate Bridge", "Golden-Gate Brdige", "San Francisco utcain", "Golden-Gate Bridge", "Golden-Gate Brdige"];
-    description: ["szoveg1", "sziveg2", "sziveg3", "sziveg4", "szoveg5", "szoveg6"],;
-
+},
+{
+    photo: "IMG_2.jpg",
+    title: "Golden-Gate bridge",
+    description: "San Francisco jellegzetes hídja, mely 3 km-en keresztül ível át az öböl felett."
+},
+{
+    photo: "IMG_3.jpg",
+    title: "Golden-Gate bridge",
+    description: "California"
+},
+{
+    photo: "IMG_4.jpg",
+    title: "Goat rock",
+    description: "California"
+},
+{
+    photo: "IMG_5.jpg",
+    title: "Yosemite National Park",
+    description: "California egyik legszebb nemzeti parkja, háttérben a méltán híres Half Dome-mal."
+},
+{
+    photo: "IMG_6.jpg",
+    title: "Hwy1",
+    description: "California híres, óceánparton húzódó országútja."
+}]
 
 $("#photo").attr("src", imagesData[currentPhoto].photo);
 $("#photo-title").append(imagesData[currentPhoto].title);
 $("#photo-description").append(imagesData[currentPhoto].description);
+
